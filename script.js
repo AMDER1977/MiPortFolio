b4662640e9016d0103de747d567c1277a7918d31
Amderson Hernandez <ahernandezdep@gmail.com>
2024-01-11 21:35:41 -0500

    "xd"

--- script.js
+++ script.js
@@ -30,7 +30,16 @@ function efectoHabilidades() {
     habilidades[2].classList.add("react");
     habilidades[3].classList.add("next");
     habilidades[4].classList.add("express");
-    habilidades[5].classList.add("postgres");
+    habilidades[6].classList.add("postgres");
+    habilidades[7].classList.add("github");
+    habilidades[8].classList.add("scrum");
+    habilidades[10].classList.add("comunicacion");
+    habilidades[11].classList.add("trabajo");
+    habilidades[12].classList.add("resolucion");
+    habilidades[13].classList.add("creatividad");
+    habilidades[14].classList.add("autodeterminacion");
+    habilidades[15].classList.add("dinamismo");
+    habilidades[16].classList.add("constancia");
   }
 }
 //primero detectar el scrolling para aplicar la animacion de la barra de habilidades:
